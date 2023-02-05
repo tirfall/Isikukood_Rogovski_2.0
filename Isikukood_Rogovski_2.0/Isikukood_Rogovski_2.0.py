99310993438ed358263020cb22f8aec66f62396e
Elena <Elena@DESKTOP-P1VF7G1>
2023-02-06 01:32:39 +0200

    Добавьте файлы проекта.

---
+++ Isikukood_Rogovski_2.0/Isikukood_Rogovski_2.0.py
@@ -0,0 +1,21 @@
+﻿from xmlrpc.client import DateTime
+from oma_moodul2 import *
+ikood=""
+while True:
+    ikood=input("Sisesta isikukood: ")
+    if pikkus(ikood)==False:
+        print("Liiga pikk või lühike isikukood")
+    else:
+        s = esimine (ikood)
+        if s=="viga":
+            print("Esimene täht ei ole õige")
+        else:
+            if sunnipaev(ikood)=="viga":
+                print("2-7 tähed on valesti sisestatud")
+            else:
+                lause(ikood)
+
+
+
+
+

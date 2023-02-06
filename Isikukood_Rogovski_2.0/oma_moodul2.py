7a4aff6530e966692e365a89039968020ed61ff3
Elena <Elena@DESKTOP-P1VF7G1>
2023-02-06 12:13:48 +0200

    1.3

--- Isikukood_Rogovski_2.0/oma_moodul2.py
+++ Isikukood_Rogovski_2.0/oma_moodul2.py
@@ -3,7 +3,7 @@ from calendar import month
 
 
 def pikkus(ikood:str)->bool:
-    if len(ikood)==11:
+    if len(ikood)==11 and ikood.isdigit():
         flag=True
     else:
         flag=False
@@ -21,17 +21,21 @@ def esimine(ikood:str)->str:
     return s
 
 def sunnipaev(ikood:str)->str:
-    ikood_list=list(map(int,ikood)) 
-    y=int(ikood_list[1])+(ikood_list[2])
-    m=int(ikood_list[3])+(ikood_list[4])
-    d=int(ikood_list[5])+(ikood_list[6])
-    if ikood_list[0] in [1,2]:
-        yy="18"
-    elif ikood_list[0] in [3,4]:
-        yy="19"
+    spaev=0
+    ikood_list=list(ikood)
+    y=ikood_list[1]+ikood_list[2]
+    m=ikood_list[3]+ikood_list[4]
+    d=ikood_list[5]+ikood_list[6]
+    if int(d)>=1 and int(d)<=31 and int(m)>=1 and int(m)<=12:
+        if int(ikood_list[0]) in [1,2]:
+            yy="18"
+        elif int(ikood_list[0]) in [3,4]:
+            yy="19"
+        else:
+            yy="20"
+        spaev=(f"{d}.{m}.{yy}{y}")
     else:
-        yy="20"
-    spaev=(f"{d}.{m}.{yy}{y}")
+        print("Viga")
     return spaev
  
 
@@ -39,31 +43,31 @@ def sunnikoht(ikood: str)->str:
     ikood_list=list(ikood) 
     t=int(ikood_list[7]+ikood_list[8]+ikood_list[9])
     
-    if 1<t<10:
+    if t>=1 and t<=10:
         haigla="Kuressaare Haigla"
-    elif 11<t<19:
+    elif t>=11 and t<=19:
         haigla="Taru Ülikooli NAistekliinik, Tartumaa, Tartu"
-    elif 21<t<220:
+    elif t>=20 and t<=220:
         haigla="Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla"
-    elif 221<t<270:
+    elif t>=221 and t<=270:
         haigla=" Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi)"
-    elif 271<t<370:
+    elif t>=271 and t<=370:
         haigla="Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla"
-    elif 371<t<420:
+    elif t>=371 and t<=420:
         haigla="Narva Haigla"
-    elif 421<t<470:
+    elif t>=421 and t<=470:
         haigla="Pärnu Haigla"
-    elif 471<t<490:
+    elif t>=471 and t<=490:
         haigla="Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla"
-    elif 491<t<520:
+    elif t>=491 and t<=520:
         haigla="Järvamaa Haigla (Paide)"
-    elif 521<t<570:
+    elif t>=521 and t<=570:
         haigla="Rakvere, Tapa haigla"
-    elif 571<t<600:
+    elif t>=571 and t<=600:
         haigla="Valga Haigla"
-    elif 601<t<650:
+    elif t>=601 and t<=650:
         haigla="Viljandi Haigla"
-    elif 651<t<700:
+    elif t>=651 and t<=700:
         haigla<="Lõuna-Eesti Haigla (Võru), Põlva Haigla"
     else:
         haigla="Välismaal"
